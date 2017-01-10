0b3a1c8b71ee72d96f417cb126fe404a7c7b7d15
luffylg <root@luffylg.cn>
2017-01-10 21:26:44 +0800

    with arraylist

--- solution_002.java
+++ solution_002.java
@@ -1,39 +1,44 @@
 package pata;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class solution_002 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		ArrayList<Double> list = new ArrayList<Double>();
 		Scanner scanner = new Scanner(System.in);
 		int K1 = scanner.nextInt();
-		int L1 = scanner.nextInt();
-		double[] val1=new double[L1+1];
-		val1[L1]=scanner.nextDouble();
-		for (int i = 1; i < K1; i++) {
-			val1[scanner.nextInt()]=scanner.nextDouble();
+		int num=scanner.nextInt();
+		while (num>=list.size()) {
+			list.add(0.0);
+		}
+		list.set(num, scanner.nextDouble());
+		for (int i = 1; i < K1; i++) {	
+			list.set(scanner.nextInt(), scanner.nextDouble());
 		}
 		int K2 = scanner.nextInt();
-		int L2 = scanner.nextInt();
-		double[] val2=new double[Math.max(L1+1, L2+1)];
-		val2[L2]=scanner.nextDouble();
+		num = scanner.nextInt();
+		while (num>=list.size()) {
+			list.add(0.0);
+		}
+		list.set(num,list.get(num)+ scanner.nextDouble());
 		for (int i = 1; i < K2; i++) {
-			val2[scanner.nextInt()]=scanner.nextDouble();
+			int index=scanner.nextInt();
+			list.set(index, list.get(index)+scanner.nextDouble());
 		}
-		for (int i = 0; i < val1.length; i++) {
-			val2[i]+=val1[i];
-		}
+		scanner.close();
 		int count=0;
-		for (int i = 0; i < val2.length; i++) {
-			if (val2[i]!=0) {
+		for (int i = 0; i < list.size(); i++) {
+			if (list.get(i)!=0) {
 				count++;
 			}
 		}
 		System.out.print(count);
-		for (int i = val2.length-1; i >=0; i--) {
-			if (val2[i]!=0) {
-				System.out.print(" "+i+" "+String.format("%.1f", val2[i]));
+		for (int i = list.size()-1; i >=0; i--) {
+			if (list.get(i)!=0) {
+				System.out.print(" "+i+" "+String.format("%.1f", list.get(i)));
 			}
 		}
 	}

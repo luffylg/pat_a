c8797e0ece07393748d11f107614ec2b0d75529c
luffylg <lake0105@foxmail.com>
2017-02-21 19:53:12 +0800

    dijkstra

--- Emergency_1003.java
+++ Emergency_1003.java
@@ -1,13 +1,12 @@
 package pata;
 
-import java.util.HashMap;
 import java.util.Scanner;
 
 public class Emergency_1003 {
-	static int count;
-	static int maxres;
+	static int[] distance;
+	static int[] count=new int[500];
+	static int[] maxres=new int[500];
 	static int INF=Integer.MAX_VALUE;
-	static int mind;
 	static int N;
 	static boolean[] visited;
 	static int[][] mMatrix;
@@ -20,15 +19,16 @@ public class Emergency_1003 {
 		int M = scanner.nextInt();
 		int C1 = scanner.nextInt();
 		int C2 = scanner.nextInt();
-		mind=INF;
 		rescuenum=new int[N];
 		for (int i = 0; i < rescuenum.length; i++) {
 			rescuenum[i]=scanner.nextInt();
 		}
 		mMatrix=new int[N][N];
 		visited=new boolean[N];
+		distance=new int[N];
 		for (int i = 0; i < N; i++) {
 			visited[i]=false;
+			distance[i]=INF;
 			for (int j = 0; j < N; j++) {
 				mMatrix[i][j]=INF;
 			}
@@ -39,32 +39,43 @@ public class Emergency_1003 {
 			mMatrix[to][from]=mMatrix[from][to]=scanner.nextInt();
 		}
 		scanner.close();
-		dfs(C1,C2,0,rescuenum[C1]);
-		System.out.println(count+" "+maxres);
+		dijkstra(C1,C2);
+		System.out.println(count[C2]+" "+maxres[C2]);
 	}
 
-	private static void dfs(int start, int end, int distance, int rescuesum) {
+	private static void dijkstra(int start, int end) {
 		// TODO Auto-generated method stub
-		if (distance>mind) {
-			return;
-		}
-		if (start!=end) {
+		int current=start;
+		int next=start;
+		maxres[current]=rescuenum[current];
+		count[current]=1;
+		while (true) {
+			if (current==end) {
+				break;
+			}
+			visited[current]=true;
+			int min=INF;
 			for (int i = 0; i < N; i++) {
-				if (!visited[i]&&mMatrix[start][i]!=INF) {
-					visited[i]=true;
-					dfs(i, end, distance+mMatrix[start][i], rescuesum+rescuenum[i]);
-					visited[i]=false;
+				if (visited[i]) {
+					continue;
+				}
+				if (mMatrix[current][i]!=INF) {
+					int tmp=distance[current]+mMatrix[current][i];
+					if (tmp<distance[i]) {
+						distance[i]=tmp;
+						maxres[i]=maxres[current]+rescuenum[i];
+						count[i]=count[current];
+					}else if (tmp==distance[i]) {
+						maxres[i]=Math.max(maxres[current]+rescuenum[i],maxres[i]);
+						count[i]+=count[current];
+					}
+				}
+				if (distance[i]<min) {
+					min=distance[i];
+					next=i;
 				}
 			}
-		}else {
-			if (distance<mind) {
-				count=1;
-				mind=distance;
-				maxres=rescuesum;
-			}else if (distance==mind) {
-				count++;
-				maxres=Math.max(rescuesum, maxres);
-			}
+			current=next;
 		}
 	}
 
